530ee3938fd4990d1e08c4a65bda410b238fd8ba
= <=>
2022-09-12 13:08:25 +0200

    logical operation buttons

--- main.js
+++ main.js
@@ -18,3 +18,33 @@ toggle.addEventListener("input", () => {
 });
 localStorage.setItem("theme", `theme-${toggle.value}`);
 container.classList.add(localStorage.getItem("theme"));
+
+const result = document.querySelector(".result input");
+const numbers = document.querySelectorAll('.keypad button[aria-label = "num"]');
+const operations = document.querySelectorAll(
+  '.keypad button[aria-label = "operation"'
+);
+const equal = document.querySelector('.keypad button[aria-label = "equal"');
+const reset = document.querySelector('.keypad button[aria-label = "reset"');
+const del = document.querySelector('.keypad button[aria-label="del"]');
+numbers.forEach((num) => {
+  num.addEventListener("click", () => {
+    result.value += num.value;
+  });
+});
+operations.forEach((op) => {
+  op.addEventListener("click", () => {
+    result.value += op.innerText;
+  });
+  equal.addEventListener("click", () => {
+    let arr = result.value.split(op);
+    result.value = eval(arr.join());
+  });
+  reset.addEventListener("click", () => {
+    result.value = "";
+  });
+  del.addEventListener("click", () => {
+    const arr = result.value.split("");
+    result.value = result.value.substring(0, arr[arr.length - 1]);
+  });
+});
